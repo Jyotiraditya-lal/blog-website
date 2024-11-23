d41e5a5665e06c09b01bfe6966a4649279b1f65e
jyotiraditya-lal <vaibhavlal0211@gmail.com>
2024-11-23 14:35:01 +0530

    your blogs page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,198 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import NavBar from "../navbar/Navbar";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
+import { Toast } from "primereact/toast";
+import LoginContext from "../store/loginCentext";
+import { Badge } from "primereact/badge";
 
-const Home=()=>{
-
-    return(
-        <React.Fragment>
-            <NavBar />
-        </React.Fragment>
-    )
-}
-
-export default Home
+const Home = () => {
+    let blogsString = localStorage.getItem("blogs");
+    let blogs = blogsString ? JSON.parse(blogsString) : [];
+    const [comments, setComments] = useState(
+      blogs.length > 0 ? blogs[blogs.length - 1].comments || [] : []
+    );
+    const [currentBlogIndex, setCurrentBlogIndex] = useState(
+      blogs.length > 0 ? blogs.length - 1 : null
+    );
+    const ctx = useContext(LoginContext);
+    const [newComment, setNewComment] = useState("");
+    const toast = useRef(null);
+  
+    const userString = localStorage.getItem("loggedinUser");
+    const user = userString ? JSON.parse(userString) : [];
+  
+    const AddComments = (e) => {
+      e.preventDefault();
+  
+      if (newComment === "") {
+        toast.current.show({
+          severity: "error",
+          summary: "Empty comment",
+          detail: "Comment cannot be empty",
+        });
+      } else {
+        const newCommentObj = {
+          author: user.name,
+          profileUrl: user.imgUrl,
+          body: newComment,
+        };
+  
+        const updatedBlogs = [...blogs];
+        updatedBlogs[currentBlogIndex].comments.push(newCommentObj);
+  
+        setComments((prevComments) => [...prevComments, newCommentObj]);
+        setNewComment("");
+        localStorage.setItem("blogs", JSON.stringify(updatedBlogs));
+      }
+    };
+  
+    const showBlog = (index) => {
+      setCurrentBlogIndex(index);
+      setComments(blogs[index].comments || []);
+    };
+  
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Toast ref={toast}></Toast>
+        <div className="flex w-full">
+          {/* Left Section: Current Blog */}
+          <div className="w-2/3 flex flex-col items-center">
+            {blogs.length === 0 && (
+              <div className="flex items-center">
+                <p>No blogs posted yet</p>
+              </div>
+            )}
+            {currentBlogIndex !== null && blogs[currentBlogIndex] && (
+              <>
+                <div className="bg-gray-200 rounded-lg shadow-md w-[57%] flex mt-5">
+                  <img
+                    src={blogs[currentBlogIndex].profileUrl}
+                    alt=""
+                    className="w-9 h-9 ml-1 mt-1 object-cover rounded-full"
+                  ></img>
+                  <p className="text-xl mt-1 ml-3">
+                    {blogs[currentBlogIndex].author}
+                  </p>
+                </div>
+                <img
+                  src={blogs[currentBlogIndex].imgUrl}
+                  alt=""
+                  className="w-2/3 h-96 object-contain"
+                ></img>
+                <div className="w-[57%] mt-5">
+                  <p className="text-xl font-bold">
+                    {blogs[currentBlogIndex].title}
+                  </p>
+                  <p className="text-lg mt-3">{blogs[currentBlogIndex].body}</p>
+                </div>
+                <div className="bg-gray-200 rounded-lg shadow-md w-[57%] flex justify-center items-center mt-5">
+                  <i className="pi pi-comment text-xl mr-2 mt-1"></i>
+                  <p className="text-xl mt-1">Comments</p>
+                  <Badge
+                    value={comments.length}
+                    severity="contrast"
+                    className="mt-1 ml-2"
+                  ></Badge>
+                </div>
+                {comments.length === 0 && (
+                  <p className="mt-5 mb-14">No comments yet</p>
+                )}
+              </>
+            )}
+  
+            {comments.length !== 0 &&
+              comments.map((comment, index) => (
+                <div
+                  className={`bg-gray-200 w-[57%] rounded-lg p-3 mt-5 ${
+                    index === comments.length - 1 ? "mb-14" : "mb-3"
+                  }`}
+                  key={index}
+                >
+                  <div className="flex">
+                    <img
+                      src={comment.profileUrl}
+                      alt=""
+                      className="w-8 h-8 object-cover rounded-full"
+                    />
+                    <p className="text-md ml-3 font-bold">{comment.author}</p>
+                  </div>
+                  <p className="text-sm mt-2">{comment.body}</p>
+                </div>
+              ))}
+  
+            {ctx.isLoggedin && (
+              <div className="fixed bottom-0 left-0 w-2/3 flex items-center justify-center shadow-lg">
+                <InputText
+                  type="text"
+                  placeholder="Write a comment..."
+                  className="w-1/2 p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  value={newComment}
+                  onChange={(e) => setNewComment(e.target.value)}
+                />
+                <Button
+                  className="ml-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                  onClick={AddComments}
+                >
+                  Post
+                </Button>
+              </div>
+            )}
+          </div>
+  
+          <div className="w-1/3 flex flex-col items-center bg-gray-100 p-4">
+            <p className="text-xl text-center px-5 mt-20 mb-7">
+              See what other people are writing about in detail and explore new
+              ideas!
+            </p>
+  
+            {(blogs.length === 0 || blogs.length === 1) && (
+              <p className="text-xl text-center mt-10">No other blogs posted</p>
+            )}
+  
+            {blogs.length > 1 &&
+              blogs.map((blog, index) => {
+                if (index !== currentBlogIndex) {
+                  return (
+                    <div
+                      className="flex mb-5 cursor-pointer w-full hover:bg-gray-200"
+                      key={index}
+                      onClick={() => showBlog(index)}
+                    >
+                      <img
+                        className="w-44 h-full object-cover"
+                        src={blog.imgUrl}
+                        alt=""
+                      />
+                      <div className="ml-4 mt-5">
+                        <p className="text-xl font-bold">{blog.title}</p>
+                        <p className="text-md text-center mt-2">A blog created by</p>
+                        <p className="text-md text-center">{blog.author}</p>
+  
+                        <span className="flex justify-center items-center mt-5">
+                          <i
+                            className="pi pi-comment mr-1 mt-0.5"
+                            title={`${blog.comments.length} comments`}
+                          ></i>
+                          <p
+                            title={`${blog.comments.length} comments`}
+                            style={{ margin: 0 }}
+                          >
+                            {blog.comments.length}
+                          </p>
+                        </span>
+                      </div>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  };
+  
+  export default Home;
+  
--- src/components/login/Logn.js
+++ src/components/login/Logn.js
@@ -3,6 +3,7 @@ import loginImg from "../../assets/loginImg.jpg";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
 import LoginContext from "../store/loginCentext";
+import NavBar from "../navbar/Navbar";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -66,7 +67,7 @@ const Login = () => {
   const submitHandler = (e) => {
     e.preventDefault();
     if (showLoginForm) {
-      ctx.login(email,pass);
+      ctx.login(email, pass);
     } else {
       const usersString = localStorage.getItem("users");
       const users = usersString ? JSON.parse(usersString) : [];
@@ -107,163 +108,172 @@ const Login = () => {
         name.length === 0));
 
   return (
-    <div className="flex h-screen">
-      <img
-        src={loginImg}
-        alt="loginImage"
-        className="w-1/2 h-full object-cover hidden md:block"
-      />
-      <div className="flex items-center justify-center w-1/2">
-        <form className="space-y-4 w-3/4 max-w-sm" onSubmit={submitHandler}>
-          {!showLoginForm && (
+    <React.Fragment>
+      <NavBar />
+
+      <div className="flex h-[90%]">
+        <img
+          src={loginImg}
+          alt="loginImage"
+          className="w-1/2 h-[90%] object-contain hidden md:block"
+        />
+        <div className="flex items-center justify-center mt-7 ml-14 md:ml-0 md:mt-0 w-1/2">
+          <form className="space-y-4 w-3/4 max-w-sm" onSubmit={submitHandler}>
+            {!showLoginForm && (
+              <div>
+                <div className="flex">
+                  <label
+                    htmlFor="name"
+                    className="block text-lg font-medium mb-2"
+                  >
+                    Name
+                  </label>
+                  <p className="text-red-600 ml-1">*</p>
+                </div>
+                <InputText
+                  id="name"
+                  type="text"
+                  placeholder="Enter your name"
+                  className="w-full p-inputtext-sm"
+                  value={name}
+                  onChange={nameHandler}
+                />
+                {nameFlag && (
+                  <p className="text-red-500">Name cannot be empty</p>
+                )}
+              </div>
+            )}
+
             <div>
               <div className="flex">
                 <label
-                  htmlFor="name"
+                  htmlFor="email"
                   className="block text-lg font-medium mb-2"
                 >
-                  Name
+                  Email
                 </label>
                 <p className="text-red-600 ml-1">*</p>
               </div>
               <InputText
-                id="name"
-                type="text"
-                placeholder="Enter your name"
+                id="email"
+                type="email"
+                placeholder="Enter your email"
                 className="w-full p-inputtext-sm"
-                value={name}
-                onChange={nameHandler}
+                value={email}
+                onChange={emailHandler}
               />
-              {nameFlag && <p className="text-red-500">Name cannot be empty</p>}
+              {emailFlag && (
+                <p className="text-red-500">Please enter a valid email</p>
+              )}
             </div>
-          )}
-
-          <div>
-            <div className="flex">
-              <label htmlFor="email" className="block text-lg font-medium mb-2">
-                Email
-              </label>
-              <p className="text-red-600 ml-1">*</p>
-            </div>
-            <InputText
-              id="email"
-              type="email"
-              placeholder="Enter your email"
-              className="w-full p-inputtext-sm"
-              value={email}
-              onChange={emailHandler}
-            />
-            {emailFlag && (
-              <p className="text-red-500">Please enter a valid email</p>
-            )}
-          </div>
-          <div>
-            <div className="flex">
-              <label
-                htmlFor="password"
-                className="block text-lg font-medium mb-2"
-              >
-                Password
-              </label>
-              <p className="text-red-600 ml-1">*</p>
-            </div>
-            <div className="relative">
-              <InputText
-                id="password"
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter your password"
-                className="w-full p-inputtext-sm pr-10"
-                value={pass}
-                onChange={passwordHandler}
-              />
-              <i
-                className={`absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer ${
-                  !showPassword ? "pi pi-eye" : "pi pi-eye-slash"
-                }`}
-                onClick={() => {
-                  setShowPassword(!showPassword);
-                }}
-              />
-            </div>
-
-            {passFlag && (
-              <p className="text-red-500">
-                Password should have at least 7 characters
-              </p>
-            )}
-          </div>
-
-          {!showLoginForm && (
-            <React.Fragment>
-              <div>
-                <div className="flex">
-                  <label
-                    htmlFor="confpassword"
-                    className="block text-lg font-medium mb-2"
-                  >
-                    Confirm Password
-                  </label>
-                  <p className="text-red-600 ml-1">*</p>
-                </div>
-                <div className="relative">
-                  <InputText
-                    id="confpassword"
-                    type={showConfirmPass ? "text" : "password"}
-                    placeholder="Confirm your password"
-                    className="w-full p-inputtext-sm"
-                    value={confirmPass}
-                    onChange={confirmPasswordHandler}
-                  />
-                  <i
-                    className={`absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer ${
-                      !showConfirmPass ? "pi pi-eye" : "pi pi-eye-slash"
-                    }`}
-                    onClick={() => {
-                      setShowConfirmPass(!showConfirmPass);
-                    }}
-                  />
-                </div>
-                {confirmPassFlag && (
-                  <p className="text-red-500">Passwords do not match</p>
-                )}
-              </div>
-              <div>
+            <div>
+              <div className="flex">
                 <label
-                  htmlFor="imageUrl"
+                  htmlFor="password"
                   className="block text-lg font-medium mb-2"
                 >
-                  Image URL
+                  Password
                 </label>
-
+                <p className="text-red-600 ml-1">*</p>
+              </div>
+              <div className="relative">
                 <InputText
-                  id="imageUrl"
-                  type="text"
-                  placeholder="Enter your image URL"
-                  className="w-full p-inputtext-sm"
-                  value={imgUrl}
-                  onChange={(e) => setImgUrl(e.target.value)}
+                  id="password"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  className="w-full p-inputtext-sm pr-10"
+                  value={pass}
+                  onChange={passwordHandler}
+                />
+                <i
+                  className={`absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer ${
+                    !showPassword ? "pi pi-eye" : "pi pi-eye-slash"
+                  }`}
+                  onClick={() => {
+                    setShowPassword(!showPassword);
+                  }}
                 />
               </div>
-            </React.Fragment>
-          )}
 
-          <div className="flex justify-center items-center">
-            <Button
-              type="submit"
-              label="Submit"
-              disabled={isButtonDisabled}
-              className="bg-blue-800 text-white px-5 py-1 mt-5"
-            />
-          </div>
-          <p
-            onClick={changeForm}
-            className="cursor-pointer text-blue-800 flex justify-center items-center"
-          >
-            {showLoginForm ? "Not a user? sign up" : "Account exists? login"}
-          </p>
-        </form>
+              {passFlag && (
+                <p className="text-red-500">
+                  Password should have at least 7 characters
+                </p>
+              )}
+            </div>
+
+            {!showLoginForm && (
+              <React.Fragment>
+                <div>
+                  <div className="flex">
+                    <label
+                      htmlFor="confpassword"
+                      className="block text-lg font-medium mb-2"
+                    >
+                      Confirm Password
+                    </label>
+                    <p className="text-red-600 ml-1">*</p>
+                  </div>
+                  <div className="relative">
+                    <InputText
+                      id="confpassword"
+                      type={showConfirmPass ? "text" : "password"}
+                      placeholder="Confirm your password"
+                      className="w-full p-inputtext-sm"
+                      value={confirmPass}
+                      onChange={confirmPasswordHandler}
+                    />
+                    <i
+                      className={`absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer ${
+                        !showConfirmPass ? "pi pi-eye" : "pi pi-eye-slash"
+                      }`}
+                      onClick={() => {
+                        setShowConfirmPass(!showConfirmPass);
+                      }}
+                    />
+                  </div>
+                  {confirmPassFlag && (
+                    <p className="text-red-500">Passwords do not match</p>
+                  )}
+                </div>
+                <div>
+                  <label
+                    htmlFor="imageUrl"
+                    className="block text-lg font-medium mb-2"
+                  >
+                    Image URL
+                  </label>
+
+                  <InputText
+                    id="imageUrl"
+                    type="text"
+                    placeholder="Enter your image URL"
+                    className="w-full p-inputtext-sm"
+                    value={imgUrl}
+                    onChange={(e) => setImgUrl(e.target.value)}
+                  />
+                </div>
+              </React.Fragment>
+            )}
+
+            <div className="flex justify-center items-center">
+              <Button
+                type="submit"
+                label="Submit"
+                disabled={isButtonDisabled}
+                className="bg-blue-800 text-white px-5 py-1 mt-5"
+              />
+            </div>
+            <p
+              onClick={changeForm}
+              className="cursor-pointer text-blue-800 flex justify-center items-center"
+            >
+              {showLoginForm ? "Not a user? sign up" : "Account exists? login"}
+            </p>
+          </form>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
